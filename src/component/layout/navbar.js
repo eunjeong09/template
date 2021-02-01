2da7a358cd2a51837ad3f612f0d40622fbd42938
eunjeong09 <ejko0911@gmail.com>
2021-02-01 20:06:09 +0900

    add collapse on left menu

--- src/component/layout/navbar.js
+++ src/component/layout/navbar.js
@@ -1,21 +1,30 @@
 import '../../style/default.sass';
+import React, { useState } from 'react';
 import { Collapse } from 'react-bootstrap';
-import { Dropdown, Accordion, Card } from 'react-bootstrap';
+import { Dropdown, Accordion, Card, Button } from 'react-bootstrap';
 
 function Navbar() {
+    const [open, setOpen] = useState(false);
+    const [openBasicUiElement, setOpenBasicUiElement] = useState(false);
+
+
     return (
       <nav >
         {/* <span class="test">sass test</span> */}
         <ul>
           <li>Dashboard</li>
-          <li>Basic UI Elements
-            <Collapse>
-              <ul>
+          <li>
+            <span onClick={() => setOpenBasicUiElement(!openBasicUiElement)}
+              aria-controls="basicUiElements"
+              aria-expanded={openBasicUiElement}>Basic UI Elements</span>
+            <Collapse in={openBasicUiElement}>
+            <ul id="basicUiElements">
                 <li>Buttons</li>
                 <li>Dropdowns</li>
               </ul>
-            </Collapse>
+          </Collapse>
           </li>
+
           <li>Form Elements</li>
           <li>Tables</li>
           <li>Icons</li>
@@ -24,18 +33,44 @@ function Navbar() {
           <li>Error Pages</li>
         </ul>
 
-        <Accordion defaultActiveKey="0">
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Basic UI Elements
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#">Buttons</Dropdown.Item>
+            <Dropdown.Item href="#">Dropdowns</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Button
+        onClick={() => setOpen(!open)}
+        aria-controls="example-collapse-text"
+        aria-expanded={open}
+        >
+          click
+        </Button>
+        <Collapse in={open}>
+          <div id="example-collapse-text">
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
+        </Collapse>
+
+        {/* <Accordion defaultActiveKey="0">
           <Card>
             <Card.Header>
             <Accordion.Toggle variant="link" eventKey="0">
-              Click me!
+              Basic UI Elements
             </Accordion.Toggle>
             </Card.Header>
             <Accordion.Collapse eventKey="0">
-              <Card.Body>Hello! I'm the body</Card.Body>
+              <Card.Body>Buttons</Card.Body>
             </Accordion.Collapse>
           </Card>
-        </Accordion>
+        </Accordion> */}
       </nav>
 
       

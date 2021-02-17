d72e96ab64cd393249b7d572dd6f073f27455eab
eunjeong09 <ejko0911@gmail.com>
2021-02-17 20:51:33 +0900

    style dropdown component

--- src/component/dropdown.js
+++ src/component/dropdown.js
@@ -1,4 +1,12 @@
-import { Dropdown, Container, Row, Col } from "react-bootstrap";
+import {
+  Dropdown,
+  DropdownButton,
+  ButtonGroup,
+  SplitButton,
+  Container,
+  Row,
+  Col,
+} from "react-bootstrap";
 
 function Dropdowns() {
   return (
@@ -17,6 +25,81 @@ function Dropdowns() {
             <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+
+        <hr />
+
+        <p>
+          DropdownButton will forward Button props to the underlying Toggle
+          component
+        </p>
+        {["Primary", "Secondary", "Success", "Info", "Warning", "Danger"].map(
+          (variant) => (
+            <DropdownButton
+              as={ButtonGroup}
+              key={variant}
+              id={`dropdown-variants-${variant}`}
+              variant={variant.toLowerCase()}
+              title={variant}
+            >
+              <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+              <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+              <Dropdown.Item eventKey="3" active>
+                Active Item
+              </Dropdown.Item>
+              <Dropdown.Divider />
+              <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+            </DropdownButton>
+          )
+        )}
+
+        <hr />
+        <Container>
+          <Row>
+            <Col>
+              <h3>Sizes</h3>
+              <p>Dropdowns work with buttons of all sizes.</p>
+              <div className="mb-2">
+                {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+                  <DropdownType
+                    as={ButtonGroup}
+                    key={idx}
+                    id={`dropdown-button-drop-${idx}`}
+                    size="lg"
+                    title="Drop large"
+                  >
+                    <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">
+                      Something else here
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                  </DropdownType>
+                ))}
+              </div>
+              <div>
+                {[DropdownButton, SplitButton].map((DropdownType, idx) => (
+                  <DropdownType
+                    as={ButtonGroup}
+                    key={idx}
+                    id={`dropdown-button-drop-${idx}`}
+                    size="sm"
+                    variant="secondary"
+                    title="Drop small"
+                  >
+                    <Dropdown.Item eventKey="1">Action</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">Another action</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">
+                      Something else here
+                    </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Item eventKey="4">Separated link</Dropdown.Item>
+                  </DropdownType>
+                ))}
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );

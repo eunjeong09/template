808fc867f73c6baf31ac5451f8027fff343c5fda
eunjeong09 <ejko0911@gmail.com>
2021-03-02 22:26:08 +0900

    Add react-fontawesome on icon component icon component에서 fontawesome 아이콘 클래스 명으로 호출

--- src/component/icon.js
+++ src/component/icon.js
@@ -2,18 +2,22 @@ import { Table, Container, Row, Col } from "react-bootstrap";
 
 //react-icons
 import { AiFillAccountBook } from "react-icons/ai";
+var FontAwesome = require("react-fontawesome");
 
 function Icon() {
   return (
     <div className="icon">
       <h2>react-icons</h2>
       <div className="bgWhite">
-        
         <Table bordered>
           <tbody>
             <tr>
-              <td><AiFillAccountBook /></td>
-              <td></td>
+              <td>
+                <AiFillAccountBook />
+              </td>
+              <td>
+                <FontAwesome className="super-crazy-colors" />
+              </td>
               <td></td>
               <td></td>
               <td></td>
